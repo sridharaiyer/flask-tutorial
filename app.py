d7b2229e8042f4c5781cac5fb5390e0b1e811117
Sridhar Iyer <iyer.a.sri@gmail.com>
2019-02-01 15:48:41 -0600

    Adoption site completed. Need to add form fields validators and Puppy ID nm in list view

---
+++ app.py
@@ -0,0 +1,13 @@
+# This is app.py, this is the main file called.
+# The app imported is the app variable from the __init__.py under the myproject directory
+from myproject import app
+from flask import render_template
+
+
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
